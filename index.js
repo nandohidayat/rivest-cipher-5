11ead8730f962c7fb56a8dae100ad744277f458b
nandohidayat <mnandohidayat@gmail.com>
2019-06-14 01:42:58 +0700

    add another rc5 for testing

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const rc5 = new RC5(key);
 
 const cipher = rc5.encrypt('sita');
 
+const rc51 = new RC5('raissa');
+
 const plain = rc5.decrypt(cipher);
 
-console.log(plain);
+console.log(plain.toString());
