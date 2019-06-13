33febaa5793b7a2add698777eef4e2eae2b17be7
nandohidayat <mnandohidayat@gmail.com>
2019-06-14 01:44:14 +0700

    reposition

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const RC5 = require('rc5');
 const key = 'nando';
 
 const rc5 = new RC5(key);
-
-const cipher = rc5.encrypt('sita');
-
 const rc51 = new RC5('raissa');
 
+const cipher = rc5.encrypt('sita');
 const plain = rc5.decrypt(cipher);
 
 console.log(plain.toString());
